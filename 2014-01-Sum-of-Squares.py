14886d8f98eacba02a3fb4dad1861fd71b6a9dee
Eric <ew398@cornell.edu>
2020-05-14 21:34:48 -0400

    Update 2014-01-Sum-of-Squares.py

--- 2014-01-Sum-of-Squares.py
+++ 2014-01-Sum-of-Squares.py
@@ -11,17 +11,17 @@ max = [4, 14, 24, 1, 4, 14, 99999//7, 124, 5]
 for index in range(len(line)):
     vars()[line[index]] = pulp.LpVariable(line[index], lowBound = 0, upBound = max[index], cat = 'Integer')
 
-problem += x[(2, 5)] - 2 * div2 == 0 #Row2
-problem += pulp.lpSum([x[(3, col)] for col in nums]) - 3 * div3 == 0 #Row3
-problem += pulp.lpSum([10 ** (5 - col) * x[(4, col)] for col in range(4, 6)]) - 4 * div4 == 0 #Row4
-problem += x[(5, 5)] - 5 * div5 == 0 #Row5
+problem += x[(2, 5)] - 2 * div2 == 0 #Row 2 Constraints
+problem += pulp.lpSum([x[(3, col)] for col in nums]) - 3 * div3 == 0 #Row 3 Constraints
+problem += pulp.lpSum([10 ** (5 - col) * x[(4, col)] for col in range(4, 6)]) - 4 * div4 == 0 #Row 4 Constraints
+problem += x[(5, 5)] - 5 * div5 == 0 #Row 5 Constraints
 
-problem += x[(5, 1)] - 2 * div6_2 == 0 #Col1
+problem += x[(5, 1)] - 2 * div6_2 == 0 #Column 1 Constraints
 problem += pulp.lpSum([x[(row, 1)] for row in nums]) - 3 * div6_3 == 0
-problem += pulp.lpSum([10 ** (5 - row) * x[(row, 2)] for row in nums]) - 7 * div7 == 0 #Col2
-problem += pulp.lpSum([10 ** (5 - row) * x[(row, 3)] for row in range(3, 6)]) - 8 * div8 == 0 #Col3
-problem += pulp.lpSum([x[(row, 4)] for row in nums]) - 9 * div9 == 0 #Col4
-problem += x[(5, 5)] == 0 #Col5
+problem += pulp.lpSum([10 ** (5 - row) * x[(row, 2)] for row in nums]) - 7 * div7 == 0 #Column 2 Constraints
+problem += pulp.lpSum([10 ** (5 - row) * x[(row, 3)] for row in range(3, 6)]) - 8 * div8 == 0 #Column 3 Constraints
+problem += pulp.lpSum([x[(row, 4)] for row in nums]) - 9 * div9 == 0 #Column 4 Constraints
+problem += x[(5, 5)] == 0 #Column 5 Constraints
 
 #Solve LP
 problem.solve()
